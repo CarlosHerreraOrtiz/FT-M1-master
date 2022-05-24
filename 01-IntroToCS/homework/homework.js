e1d1c9bdfd98d40cff4fca970eea5523f9ff8ae4
CarlosHerreraOrtiz <carlosherreraortiz@gmail.com>
2022-05-23 23:03:32 -0500

    desarrollo homework 1

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,12 +2,23 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  var decimal = 0;
+  for (var i=0; i<num.length; i++){
+    decimal = decimal + num[i] * 2 ** (num.length-1-i);
+  }
+  return decimal;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  var binario = "";
+  do {
+    binario = (num % 2) + binario;
+    //console.log (binario);
+    num = Math.trunc(num/2);
+    //console.log (num);
+    } while (num >0);
+    return binario;
 }
 
 
